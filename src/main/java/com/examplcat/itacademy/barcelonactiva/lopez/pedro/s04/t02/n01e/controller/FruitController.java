b1b9b1c7423438d92c8161c4362eef31027527e9
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2023-12-19 11:00:39 +0100

    inject FruitService object to the endpoint controller;

--- src/main/java/com/examplcat/itacademy/barcelonactiva/lopez/pedro/s04/t02/n01e/controller/FruitController.java
+++ src/main/java/com/examplcat/itacademy/barcelonactiva/lopez/pedro/s04/t02/n01e/controller/FruitController.java
@@ -1,10 +1,16 @@
 package com.examplcat.itacademy.barcelonactiva.lopez.pedro.s04.t02.n01e.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.examplcat.itacademy.barcelonactiva.lopez.pedro.s04.t02.n01e.service.FruitService;
 
 @RestController
 @RequestMapping("fruits")
 public class FruitController {
+	
+	@Autowired
+	FruitService fruitService;
 	
 }
