efdc09786c559488925887355400483ad07a09ec
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2023-12-19 10:54:55 +0100

    creates FruitController class and uses the bind annotations to
    1) set the object up as endpoint resources controller
    
    2) set the endpoint path access up

---
+++ src/main/java/com/examplcat/itacademy/barcelonactiva/lopez/pedro/s04/t02/n01e/controller/FruitController.java
@@ -0,0 +1,10 @@
+package com.examplcat.itacademy.barcelonactiva.lopez.pedro.s04.t02.n01e.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("fruits")
+public class FruitController {
+	
+}
