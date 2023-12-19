978a8f5b068ef04f0c9e6903de977d1ae5b2f7e7
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2023-12-19 09:55:56 +0100

    uses jakarta.persistance annotation to set
    the class up as an entity, as a table and annotates the attribute Id as
    primary key, using a specific strategy;
    
    Signed-off-by: Pedro Lopez <p.lopez.ch.hr@hotmail.com>

--- src/main/java/com/examplcat/itacademy/barcelonactiva/lopez/pedro/s04/t02/n01e/domain/Fruit.java
+++ src/main/java/com/examplcat/itacademy/barcelonactiva/lopez/pedro/s04/t02/n01e/domain/Fruit.java
@@ -1,9 +1,53 @@
 package com.examplcat.itacademy.barcelonactiva.lopez.pedro.s04.t02.n01e.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frutas")
 public class Fruit {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int kilos;
+	
+	public Fruit() {
+		
+	}
+
+	public Fruit(String name, int kilos) {
+		this.name = name;
+		this.kilos = kilos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKilos() {
+		return kilos;
+	}
+
+	public void setKilos(int kilos) {
+		this.kilos = kilos;
+	}
+
+	@Override
+	public String toString() {
+		return "Fruit [id=" + id + ", name=" + name + ", kilos=" + kilos + "]";
+	}
 
 }
